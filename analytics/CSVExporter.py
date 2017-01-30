1d586b53ae2228ff648e2a1c3c31f0e55062a350
Joshua Haley <Joshua.haley@knights.ucf.edu>
2017-01-30 16:03:05 -0500

    Cleaned up the process for getting user authorization.

--- analytics/CSVExporter.py
+++ analytics/CSVExporter.py
@@ -1,28 +1,18 @@
 from stravalib.client import Client
-import strava_common.common as common
-import requests
-import webbrowser
+import strava_common.Authorize as authorize
+import datetime
+
+username= "joshua_haley"
+start=datetime.datetime(2017,1,1,0, 0, 0)
+
 
 def main():
-    client = Client()
-    authorize_url = client.authorization_url(client_id=13057, redirect_uri='http://127.0.0.1:5000/authorized')
-
-    webbrowser.open(authorize_url);
-    code ="null"
-    while code =="null":
-        print("waiting for code")
-        response = requests.get("http://localhost:5000/code")
-        if response.status_code == 200:
-            code = response.content
-
-    access_token = client.exchange_code_for_token(client_id=13057,
-                                                  client_secret='b48d4a0be5e16f97b4e52060252afe4e5bf1293f',
-                                                  code=code)
-
-    # Now store that access token somewhere (a database?)
-    client.access_token = access_token
+    client = authorize.get_authorized_client(username)
     athlete = client.get_athlete()
-    print("For {id}, I now have an access token {token}".format(id=athlete.id, token=access_token))
+    print("{id} ".format(id=athlete.username))
+    activities = client.get_activities(after=start)
+    for activity in activities:
+        print(activity)
 
 def open_file():
     print("Not yet Implemented")
---
+++ strava_common/Authorize.py
@@ -0,0 +1,57 @@
+from stravalib.client import Client
+from flask import Flask
+from flask import request
+import multiprocessing
+import webbrowser
+import requests
+
+
+
+def get_authorized_client(username):
+    client= Client()
+    token = get_access_token(username, client)
+    client.access_token = token
+    return client
+
+def get_access_token(username, client):
+    #TODO: Lookup Cached Result
+    server_thread = multiprocessing.Process(target=setup_server)
+    server_thread.start()
+    authorize_url = client.authorization_url(client_id=13057, redirect_uri='http://127.0.0.1:5000/authorized')
+
+    webbrowser.open(authorize_url);
+    code = "null"
+    while code == "null":
+        print("waiting for code")
+        response = requests.get("http://localhost:5000/code")
+        if response.status_code == 200:
+            code = response.content
+
+    server_thread.terminate()
+
+    access_token = client.exchange_code_for_token(client_id=13057,
+                                                  client_secret='b48d4a0be5e16f97b4e52060252afe4e5bf1293f',
+                                                  code=code)
+
+    #TODO: Save Client access Token
+    return access_token
+
+
+def setup_server():
+    app = Flask(__name__)
+    code = "No Authorized"
+
+    @app.route('/authorized', methods=['GET'])
+    def authorized():
+        nonlocal code
+        code = request.args.get('code', '')
+        return "Authorized!"
+
+    @app.route('/code', methods=['GET'])
+    def get_code():
+        return str(code)
+
+    app.run(debug=False)
+
+if __name__ == "__main__":
+    setup_server()
