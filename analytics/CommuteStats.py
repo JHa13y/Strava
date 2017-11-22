fb93074ce8e56ec597c815bcd1377d03b733d155
Joshua Haley <Joshua.haley@knights.ucf.edu>
2017-11-22 11:18:52 -0500

    Fixed issue with bikes being deleted/retired from strava causing issues.

--- analytics/CommuteStats.py
+++ analytics/CommuteStats.py
@@ -21,6 +21,7 @@ def main():
     athlete = client.get_athlete()
     bike_distance_breakdown ={}
     bikes={}
+    bike_distance_breakdown["other"] = 0
     for b in athlete.bikes:
         bikes[b.id] = b.name
         bike_distance_breakdown[b.id] = 0
@@ -30,7 +31,10 @@ def main():
     total_distance=0 ##In Meters
     for act in acts:
         total_distance += act.distance._num
-        bike_distance_breakdown[act.gear_id] += act.distance._num
+        if act.gear_id in bike_distance_breakdown:
+            bike_distance_breakdown[act.gear_id] += act.distance._num
+        else:
+            bike_distance_breakdown["other"] += act.distance._num
 
     d_km = total_distance/1000;
     d_miles = d_km * 0.621371;
@@ -42,8 +46,11 @@ def main():
     print("You used the following gear to commute: ")
 
     for b in athlete.bikes:
-        dist = bike_distance_breakdown[b.id]/1000.0 * 0.621371;
-        print('{:6.2f}'.format(dist) + " miles on " + bikes[b.id])
+        if b.id in bike_distance_breakdown:
+            dist = bike_distance_breakdown[b.id]/1000.0 * 0.621371;
+            print('{:6.2f}'.format(dist) + " miles on " + bikes[b.id])
+    dist = bike_distance_breakdown["other"] / 1000.0 * 0.621371;
+    print('{:6.2f}'.format(dist) + " miles on Other")
 
     print("Data Source: https://ecf.com/sites/ecf.com/files/ECF_CO2_WEB.pdf")
     print("Script Source: https://github.com/JHa13y/Strava")
